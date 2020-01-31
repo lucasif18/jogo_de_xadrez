c788869353c59bbac16591594899a38aa7129046
Micael Oliveira <micael_oliveira_93@hotmail.com>
2020-01-31 14:31:32 +0000

    class posicao

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,13 @@
 package application;
 
+import bordgame.Posicao;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
-		System.out.println("ola");
+		Posicao pos = new Posicao (3,5);
+		System.out.println(pos);
 
 	}
 
